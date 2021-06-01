d594743a241e3e60edf443c4186a5a97687a5a13
albertosanchezmateo <35465627+albertosanchezmateo@users.noreply.github.com>
2021-06-02 00:04:16 +0200

    Nombre y Color añadidos

---
+++ PolePosition2.0.2/Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager
+{
+
+    #region Client
+        public override void OnClientConnect(NetworkConnection conn){
+           base.OnClientConnect(conn);
+
+            Debug.Log("Cliente conectado");
+        }
+
+      /*  public override void AddPlayer(){
+
+        }*/
+
+    #endregion
+
+    #region Server
+
+        public override void OnServerConnect(NetworkConnection conn){
+            Debug.Log("Nueva connección okay");
+        }
+
+
+    #endregion
+
+}
---
+++ PolePosition2.0.2/Assets/Scripts/UIManager.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public bool showGUI = true;
+
+    private MyNetworkManager m_NetworkManager;
+
+    [Header("Main Menu")] [SerializeField] private GameObject mainMenu;
+    [SerializeField] private Button buttonHost;
+    [SerializeField] private Button buttonClient;
+    [SerializeField] private Button buttonServer;
+    [SerializeField] private InputField inputFieldIP;
+
+    [Header("In-Game HUD")] [SerializeField]
+    private GameObject inGameHUD;
+
+    [SerializeField] private Text textSpeed;
+    [SerializeField] private Text textLaps;
+    [SerializeField] private Text textPosition;
+
+    [Header("Selección De Nombre")] 
+
+    [SerializeField] private GameObject menuSeleccionNombre;
+    [SerializeField] private InputField inputFieldNombreJugador;
+    [SerializeField] private Button buttonAceptarNombre;
+    [SerializeField] private Button buttoncolor1;
+    [SerializeField] private Button buttoncolor2;
+    [SerializeField] private Button buttoncolor3;
+    [SerializeField] private Button buttoncolor4;
+    public bool [] buttonActivado = new bool [4];
+
+
+    private void Awake()
+    {
+        m_NetworkManager = FindObjectOfType<MyNetworkManager>();
+    }
+
+    private void Start()
+    {
+        buttonHost.onClick.AddListener(() => StartHost());
+        buttonClient.onClick.AddListener(() => StartClient());
+        buttonServer.onClick.AddListener(() => StartServer());
+        buttonAceptarNombre.onClick.AddListener(() => comprobarNombre());
+        buttoncolor1.onClick.AddListener(() => cambiarColor(1));
+        buttoncolor2.onClick.AddListener(() => cambiarColor(2));
+        buttoncolor3.onClick.AddListener(() => cambiarColor(3));
+        buttoncolor4.onClick.AddListener(() => cambiarColor(4));
+        ActivateMainMenu();
+
+        for(int i =0; i<buttonActivado.Length;i++){
+            buttonActivado[i] = true;
+        }
+    }
+
+    public void UpdateSpeed(int speed)
+    {
+        textSpeed.text = "Speed " + speed + " Km/h";
+    }
+
+    private void ActivateMainMenu()
+    {
+        mainMenu.SetActive(true);
+        inGameHUD.SetActive(false);
+        menuSeleccionNombre.SetActive(false);
+    }
+
+    private void ActivateInGameHUD()
+    {
+        mainMenu.SetActive(false);
+        inGameHUD.SetActive(true);
+        menuSeleccionNombre.SetActive(false);
+    }
+
+    private void ActivateSeleccionNombre()
+    {
+        mainMenu.SetActive(false);
+        inGameHUD.SetActive(false);
+        menuSeleccionNombre.SetActive(true);
+    }
+
+  
+    private void StartHost()
+    {
+        m_NetworkManager.StartHost();
+        ActivateSeleccionNombre();
+    }
+
+    private void StartClient()
+    {
+        m_NetworkManager.StartClient();
+        m_NetworkManager.networkAddress = inputFieldIP.text;
+        ActivateSeleccionNombre();
+    }
+
+    private void StartServer()
+    {
+        m_NetworkManager.StartServer();
+        ActivateInGameHUD();
+    }
+
+    private void comprobarNombre(){
+        if(inputFieldNombreJugador.text != ""){
+            SetupPlayer[] players = (SetupPlayer[]) GameObject.FindObjectsOfType (typeof(SetupPlayer));
+            foreach(SetupPlayer player in players){
+                if(player.hasAuthority){
+                    player.CmdSetName(inputFieldNombreJugador.text);
+                }
+            }
+        }
+    }
+
+    private void cambiarColor(int color){
+        if(buttonActivado[color-1]){
+            Color colorNuevo = new Color(0f,0f,0f);
+            switch(color){
+                case 1:
+                    colorNuevo = new Color(255f,0f, 0f);
+                    break;
+                case 2:
+                    colorNuevo = new Color(255f,227f,0f);
+                    break;
+                case 3:
+                    colorNuevo = new Color(0f,255f,0f);
+                    break;
+                case 4:
+                    colorNuevo = new Color(0f,0f,255f);
+                    break;
+            }
+              SetupPlayer[] players = (SetupPlayer[]) GameObject.FindObjectsOfType (typeof(SetupPlayer));
+        foreach(SetupPlayer player in players){
+                if(player.hasAuthority){
+                    player.CmdSetColor(colorNuevo);
+                    
+                }
+            }
+        }
+      
+    }
+}
