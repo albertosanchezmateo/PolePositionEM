8c2aa8c466693c185be86cf04790ce3c97441b16
albertosanchezmateo <35465627+albertosanchezmateo@users.noreply.github.com>
2021-06-02 23:41:50 +0200

    Checkpoints Terminados + inicio contador vueltas y marcha atras

--- PolePosition2.0.2/Assets/Scripts/PlayerInfo.cs
+++ PolePosition2.0.2/Assets/Scripts/PlayerInfo.cs
@@ -14,7 +14,10 @@ public class PlayerInfo : MonoBehaviour
     [SerializeField] private int numCheckPoint;
     [SerializeField] private bool chocado;
     private Vector3[] posicionesChecks = new Vector3[4];
+    private Vector3[] posicionesLookAt = new Vector3[4];
 
+    [SerializeField] public bool sentidoContrario = false;
+    [SerializeField]private int vueltas = 0;
     private void Awake()
     {
         check1 = GameObject.Find("CheckPoint1");
@@ -29,7 +32,13 @@ public class PlayerInfo : MonoBehaviour
         check4 = GameObject.Find("CheckPoint4");
         posicionesChecks[3] = check4.transform.position;
 
-        numCheckPoint = 1;
+        posicionesLookAt[0] = GameObject.Find("LookAt1").transform.position;
+        posicionesLookAt[1] = GameObject.Find("LookAt2").transform.position;
+        posicionesLookAt[2] = GameObject.Find("LookAt3").transform.position;
+        posicionesLookAt[3] = GameObject.Find("LookAt4").transform.position;
+
+
+        numCheckPoint = 4;
         chocado = false;
     }
 
@@ -37,34 +46,57 @@ public class PlayerInfo : MonoBehaviour
     {
         Vector3 jugador = this.GetComponent<Transform>().position;
 
-        Debug.Log(jugador);
+       
 
         float distancia1 = Vector3.Distance(posicionesChecks[0], jugador);
         float distancia2 = Vector3.Distance(posicionesChecks[1], jugador);
         float distancia3 = Vector3.Distance(posicionesChecks[2], jugador);
         float distancia4 = Vector3.Distance(posicionesChecks[3], jugador);
-
-        if (distancia1 <= 10)
-        {
-            numCheckPoint = 1;
+        
+        if (distancia1 <= 10){
+            if(numCheckPoint !=4){
+                sentidoContrario = true;
+            }else{
+                numCheckPoint = 1;
+                sentidoContrario = false;
+                vueltas++;
+            }   
         }
 
         if (distancia2 <= 10)
         {
-            numCheckPoint = 2;
+            if(numCheckPoint !=1){
+                sentidoContrario = true;
+            }else{
+                numCheckPoint = 2;
+                sentidoContrario = false;
+            }  
+            
         }
 
         if (distancia3 <= 10)
         {
-            numCheckPoint = 3;
+            if(numCheckPoint !=2){
+                sentidoContrario = true;
+            }else{
+                numCheckPoint = 3;
+                sentidoContrario = false;
+            }  
         }
 
         if (distancia4 <= 10)
         {
-            numCheckPoint = 4;
-        }
+            if(numCheckPoint !=3){
+                sentidoContrario = true;
+            }else{
+                numCheckPoint = 4;
+                sentidoContrario = false;
+            }  
+        } 
+        
+       
 
-        if (Mathf.Abs(this.GetComponent<Transform>().rotation.z) >= 180)
+        if (((Mathf.Abs(this.GetComponent<Transform>().localEulerAngles.z)>90)&&Mathf.Abs(this.GetComponent<Transform>().localEulerAngles.z)<280) ||((Mathf.Abs(this.GetComponent<Transform>().localEulerAngles.x)>90)&&Mathf.Abs(this.GetComponent<Transform>().localEulerAngles.x)<280))
         {
             chocado = true;
         }
@@ -72,8 +104,12 @@ public class PlayerInfo : MonoBehaviour
         if (chocado)
         {
             this.GetComponent<Transform>().position = posicionesChecks[numCheckPoint-1];
+            this.GetComponent<Transform>().LookAt(posicionesLookAt[numCheckPoint-1]);
+            chocado = false;
         }
-
+        
+        Debug.Log("Rotation X:"+ Mathf.Abs(this.GetComponent<Transform>().localEulerAngles.x)) ;
+        Debug.Log("Rotation Z:" + Mathf.Abs(this.GetComponent<Transform>().localEulerAngles.z));
     }
     public string Name { get; set; }
 
